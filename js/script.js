12128f3c5598ef92abc3dd67417d8f96bc2027cb
Rashed Hossain <145587722+rashedhossain25@users.noreply.github.com>
2024-01-02 23:42:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+//jq code
+
+$(document).ready(function(){
+    var countdown = $("#countdown").countdown360({
+
+        radius      : 60,
+    
+        seconds     : 100,
+    
+        fontColor   :'#FFFFFF',
+    
+        autostart   : true,
+    
+        onComplete  :function () { console.log('done') }
+    
+    
+    });
+    
+    var countdown = $("#countdow").countdown360({
+    
+        radius      : 60,
+    
+        seconds     : 100,
+    
+        fontColor   :'#FFFFFF',
+    
+        autostart   : true,
+    
+        onComplete  :function () { console.log('done') }
+    
+    
+    });
+    
+    var countdown = $("#countdownd").countdown360({
+    
+        radius      : 60,
+    
+        seconds     : 100,
+    
+        fontColor   :'#FFFFFF',
+    
+        autostart   : true,
+    
+        onComplete  :function () { console.log('done') }
+    
+    
+    });
+});
